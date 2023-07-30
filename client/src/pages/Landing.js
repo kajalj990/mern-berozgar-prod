36e0621fa3ead7ec78e363df2ecc2b472a6e1ef4
kajaljaiswal07 <80945107+kajaljaiswal07@users.noreply.github.com>
2023-07-30 18:58:59 +0530

    changed description

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -20,13 +20,10 @@ const Landing = () => {
               Job <span>tracking</span> app
             </h1>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              A job tracking app allows users to keep track of their job
+              applications, including the status of each application, deadlines,
+              and important details about each job. It helps users stay
+              organized and on top of their job search.
             </p>
             <Link to="/register" className="btn btn-hero">
               Login / Register
